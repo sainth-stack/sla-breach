fcca7c00c10052a6a7a9018b08796eb0b6887bac
sainath-stylishop <sainath@stylishop.com>
2025-08-18 17:56:29 +0530

    updated

--- src/const.js
+++ src/const.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-export const baseURL = 'http://127.0.0.1:8000';
+// export const baseURL = 'http://127.0.0.1:8000';
 
+export const baseURL = 'http://34.123.109.79:3001';
 // Central axios instance configured with baseURL
 const api = axios.create({ baseURL });
 
--- src/index.js
+++ src/index.js
@@ -3,29 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { PublicClientApplication } from '@azure/msal-browser';
-import { MsalProvider } from '@azure/msal-react';
-
-const msalConfig = {
-  auth: {
-    clientId: process.env.REACT_APP_SHAREPOINT_CLIENT_ID || 'YOUR_CLIENT_ID',
-    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_SHAREPOINT_TENANT_ID || 'common'}`,
-    redirectUri: window.location.origin,
-  },
-  cache: {
-    cacheLocation: 'localStorage',
-    storeAuthStateInCookie: true,
-  },
-};
-
-const pca = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MsalProvider instance={pca}>
-      <App />
-    </MsalProvider>
+    <App />
   </React.StrictMode>
 );
 
