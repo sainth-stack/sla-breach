e48112e1c56b8b2ae074f40e8a33802b5e649b2a
sainath-stylishop <sainath@stylishop.com>
2026-02-04 22:26:32 +0530

    updated changes

--- src/const.js
+++ src/const.js
@@ -1,4 +1,5 @@
-export const baseURL='https://backend-ams-fastapi.cfapps.us10-001.hana.ondemand.com/api'
+export const baseURL = 'https://backend-ams-fastapi.cfapps.us10-001.hana.ondemand.com/api'
 // export const baseURL='http://localhost:8000/api'
 
 export const systemMonitoringHistoryURL = 'https://ans-webhook-happy-civet-oc.cfapps.us10-001.hana.ondemand.com/api/history';
+export const batchMonitorURL = 'https://tg-monitoring-backend.cfapps.us10-001.hana.ondemand.com/v1/automate/idoc-data/status';
